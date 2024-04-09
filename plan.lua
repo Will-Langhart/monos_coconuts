95244b1be1ce6b9dbba949da831a257d8d0d92dd
Will Langhart <theholotrout@gmail.com>
2024-04-09 16:03:20 -0400

    Create plan.lua

---
+++ plan.lua
@@ -0,0 +1,17 @@
+Costs:
+  ~ wix website purchase domain + plan on me ($39/month for 12 months for domain name and server as well as mainenance and building)
+  ~ deployment labor on Mono ($380 | 8 hours at $50/hour)
+  ~ stripe customer payment plan on Mono ($400 + $39/month)
+
+====$400 building labor and software assets + $39/month for server hosting and domain name whenever ready to launch site
+
+Dev Ops Blueprint:
+  ~ google maps mobile location
+  ~ photos in website 
+  ~ number in website 
+  ~ ig + tiktok -----> integrated w/website
+  ~ shirts and other complimentary products for vwebsite 
+  ~ woxo custom embed ----> website 
+
+CI/CD Post Deployment Blueprint:
+  ~
